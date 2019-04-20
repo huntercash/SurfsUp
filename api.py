35c6349518dbe1eee1c270aad04f7be80dc5a392
Hunter Cash <hmclaug@gmail.com>
2019-04-19 21:56:51 -0500

    add more API links

--- api.py
+++ api.py
@@ -34,6 +34,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -67,18 +68,58 @@ def precipitation():
 
     return jsonify(precipitation_list)
 
+# @app.route("/api/v1.0/justice-league")
+# def justice_league():
+#     """Return the justice league data as json"""
+
+#     return jsonify(justice_league_members)
 
 
-# @app.route("/api/stations")
-#     return jsonify(all_passengers)
+@app.route("/api/stations")
+def station():
+    """Returns All of The Stations in Hawaii"""
+    statn = session.query(Station.station)
+    station_list = []
+    for station in statn:
+        station_dict = {}
+        station_dict['station'] = station
+        station_list.append(station_dict)
+    return jsonify(station_list)
 
-# @app.route("/api/temperature")
-#     return jsonify()
 
-# @app.route("/api/<start>")
-#     return jsonify()
 
-# @app.route("/api/start-end")
-#     return jsonify()
+
+@app.route("/api/temperature")
+def tobs():
+    """Return a list of temperatures for prior year"""
+# Query for the dates and temperature observations from the last year.
+# Convert the query results to a Dictionary using `date` as the key and `tobs` as the value.
+# Return the json representation of your dictionary.
+    temp = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= dt.date(2017, 8, 23) - dt.timedelta(days=365)).\
+        order_by(Measurement.date).all()
+# Create a list of dicts with `date` and `tobs` as the keys and values
+    tobs_summary = []
+    for date, tobs in temp:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        tobs_summary.append(tobs_dict)
+
+    return jsonify(tobs_summary)
+
+
+
+
+# /api/<start> and /api/<start>/<end>
+
+# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+
+# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+
+# Hint: You may want to look into how to create a defualt value for your route variable.
+
+# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+
 if __name__ == '__main__':
     app.run(debug=True)
